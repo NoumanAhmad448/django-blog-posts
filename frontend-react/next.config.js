0cc376caa995220d5cb0745beddfd5b26a9c6830
noumanahmad448 <nouman.ahmad448@gmail.com>
2023-10-16 14:04:11 +0800

    removed all webpack config

--- frontend-react/next.config.js
+++ frontend-react/next.config.js
@@ -1,17 +1,17 @@
 module.exports = {
-    webpack(config) {
-      config.infrastructureLogging = { debug: /PackFileCache/ }
-      config.resolve.fallback = {
-        // if you miss it, all the other options in fallback, specified
-        // by next.js will be dropped.
-        ...config.resolve.fallback,
+    // webpack(config) {
+    //   config.infrastructureLogging = { debug: /PackFileCache/ }
+    //   config.resolve.fallback = {
+    //     // if you miss it, all the other options in fallback, specified
+    //     // by next.js will be dropped.
+    //     ...config.resolve.fallback,
 
-        fs: false,
-        path: false // the solution
-      }
-      return config;
-    },
-    generateBuildId: async () => {
-        return "fjdafjd;fjd;afjd;fja;fdjs;fjsd;laj;l3j24l324j23l4j23l4jl324j23l4j2ldsn;faljdsl;fjl54354075405740574"
-    },
+    //     fs: false,
+    //     path: false // the solution
+    //   }
+    //   return config;
+    // },
+    // generateBuildId: async () => {
+    //     return "fjdafjd;fjd;afjd;fja;fdjs;fjsd;laj;l3j24l324j23l4j23l4jl324j23l4j2ldsn;faljdsl;fjl54354075405740574"
+    // },
 }
